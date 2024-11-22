1f819c3a54a5a37d1c842bb3beab73ef173ab1fc
larry <liularry2023@gmail.com>
2024-11-22 11:18:49 -0500

    new model method attempt

---
+++ src/testing_rgbv2.py
@@ -0,0 +1,54 @@
+import cv2
+import os 
+import numpy as np 
+import pandas as pd 
+from PIL import Image
+from tensorflow.keras.models import load_model
+from sklearn.metrics import classification_report 
+
+# function to load and preprocess test images 
+
+def preprocess_test_images(image_paths):
+    images = []
+    for image_path in image_paths:
+        if os.path.exists(image_path):
+           image = cv2.imread(image_path)
+           image = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
+           image = cv2.resize (image , (224,224))
+           image_array = np.array(image) / 255.00 # this will normalize the image to a value between 0 and 1 (this will simpify the calculations)
+           images.append(image_array)
+        else:
+            print(f"image file {image_path} not found.")
+    return np.array(images)
+spreadsheet_path = 'Data/PatientsTestingData.xlsx'
+spreadsheet = pd.read_excel(spreadsheet_path)
+
+# Extract files paths from the spreadsheet 
+test_image_paths = spreadsheet['Patient X-Ray File'].tolist()
+test_images = preprocess_test_images(test_image_paths)
+
+# load the trained model 
+
+model = load_model('best_pneumonia_modelrgbv2.keras')
+#model = load_model('87_85.keras') # this uses the image size of 224,224,3
+#model = load_model('90_03.keras') # this uses the image size of 448,448,3
+
+# make predictions 
+
+predictions = model.predict(test_images)
+
+# get the predicted classes (index of the highest probability)
+
+predicted_classes = np.argmax(predictions , axis = 1 )
+
+# print out the predictions 
+
+for i , image_path in enumerate(test_image_paths):
+    print(f"\Image: {image_path} , Predicted Class: {predicted_classes[i]}")
+true_labels = spreadsheet['Pneumonia'].values
+print(f"\ntrue outcome {spreadsheet['Pneumonia']}")
+
+#calculate the accuracy 
+
+accuracy = np.mean(predicted_classes == true_labels)
+print(f"Accuracy: {accuracy * 100:.2f}%")
--- src/training_RGBv2.py
+++ src/training_RGBv2.py
@@ -5,10 +5,11 @@ import numpy as np # this is the libary for numerical operations
 import pandas as pd # this is the libary for data manipulation 
 import tensorflow as tf # this is the libary for deep learning (machine learning/ neural networks)
 from tensorflow.keras.models import Model , Sequential # these are models that will be called on later in the code
-from tensorflow.keras.layers import Dense, Flatten , Conv2D, MaxPooling2D , Input , Dropout , BatchNormalization, Activation # these are the input , hidden and output layers within the CNN model 
+from tensorflow.keras.layers import Dense, Flatten , Conv2D, MaxPooling2D , Input , Dropout , BatchNormalization, Activation , GlobalAveragePooling2D, Add, Concatenate # these are the input , hidden and output layers within the CNN model 
 from tensorflow.keras.optimizers import Adam # this is the optimization function 
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint , ReduceLROnPlateau
 from tensorflow.keras.preprocessing.image import ImageDataGenerator 
+from tensorflow.keras.regularizers import l2
 from sklearn.model_selection import train_test_split # this is the library that is used to split data into a training and testing subset 
 from sklearn.preprocessing import LabelEncoder # this is the function to encode labels 
 from sklearn.metrics import classification_report # this is the function that is used to generate a classification report 
@@ -27,8 +28,6 @@ def preprocess_data(spreadsheet, dir_path, num_records=5200 , target_size= (224,
             break
         xray_file = row['Patient X-Ray File']
         pneumonia = row['Pneumonia']
-        if pneumonia ==2:
-            pneumonia =1
         # Load the X-ray Image 
         image_path = os.path.join(dir_path , xray_file)
         if os.path.exists(image_path):
@@ -45,10 +44,7 @@ def preprocess_data(spreadsheet, dir_path, num_records=5200 , target_size= (224,
 # Preprocess the data
 images, labels = preprocess_data(spreadsheet, DIR, num_records=5200, target_size=(224, 224))
 # Encode labels
-
 label_encoder = LabelEncoder()
-if label_encoder ==2:
-    label_encoder =1
 encoded_labels = label_encoder.fit_transform(labels)
 num_classes = len(np.unique(encoded_labels))
 # Split the data into training, validation, and testing sets
@@ -65,15 +61,74 @@ datagen = ImageDataGenerator(
     height_shift_range=0.15,
     zoom_range=0.2,
     horizontal_flip=True
+    #fill_mode='nearest',
+   # brightness_range=[0.8,1.2],
+   # shear_range=0.1
 )
 datagen.fit(X_train)
+
+def build_modelv2(input_shape= (224,224,3) , num_classes =3 ):
+     inputs = Input(shape = input_shape)
+    
+     # this is the first convolutional block
+     conv1 = Conv2D(32 , (5,5) , strides = 1 , padding ='same' )(inputs)
+     bn1 = BatchNormalization()(conv1)
+     a1 = Activation('relu')(bn1)
+     conv2 = Conv2D(32, (5,5) , strides =1 , padding ='same')(a1) 
+     bn2 = BatchNormalization()(conv2)
+     res1 = Add()([bn1, bn2])
+     a2 = Activation('relu')(res1)
+     mpool1 = MaxPooling2D(2,2)(a2)
+    # this is the second convolutional block
+     conv3 = Conv2D(64 , (4,4) , strides =1 , padding = 'same')(mpool1)
+     bn3 = BatchNormalization()(conv3)
+     a3 = Activation('relu')(bn3)
+     conv4 = Conv2D(64, (4,4) , strides = 1 , padding ='same')(a3) 
+     bn4 = BatchNormalization()(conv4)
+     res2 = Add()([bn3, bn4])
+     a4 = Activation('relu')(res2)
+     mpool2 = MaxPooling2D(2,2)(a4)
+    # third convolutional block 
+     conv5 = Conv2D(128,(3,3) , strides =1 , padding ='same')(mpool2)
+     bn5 = BatchNormalization()(conv5)
+     a5 = Activation('relu')(bn5)
+     conv6 = Conv2D(128,(3,3) , strides =1 , padding ='same')(a5)
+     bn6 = BatchNormalization()(conv6)
+     res3 = Add()[(bn5 , bn6)]
+     a6 = Activation('relu')(res3)
+     mpool3 = MaxPooling2D(3,3)(a6)
+     # For the final layer a parallel path will be used 
+    # first path 
+     conv7 = Conv2D(256 , (3,3) , strides = 1, padding ='same')(mpool3)
+     bn7 = BatchNormalization()(conv7)
+     a7 = Activation('relu')(bn7)
+     # second path 
+     conv8 = Conv2D(256 , (1,1), strides = 1 , padding ='same')(mpool3)
+     bn8 = BatchNormalization()(conv8)
+     a8 = Activation('relu')(bn8)
+     #merge the two paths into one using concat keras function 
+     Merge = Concatenate()([a7, a8])
+     mpool4 = MaxPooling2D((2,2))(Merge)
+
+     flat = GlobalAveragePooling2D()(mpool4)
+     dense1 = Dense(512, activation='relu', kernel_regularizer=l2(0.01))(flat)
+     drop1 = Dropout(0.5)(dense1)
+     dense2 = Dense(256, activation='relu', kernel_regularizer=l2(0.01))(drop1)
+     drop2 = Dropout(0.3)(dense2)
+    
+     outputs = Dense(num_classes, activation='softmax')(drop2)
+     model = Model(inputs, outputs)
+     return model
+
+'''
 # Build the model based on Table 1 from the paper
 def build_model(input_shape=(224, 224, 3), num_classes=3):  # Change num_classes to 3
     inputs = Input(shape=input_shape)
-    x = Conv2D(32, (3,3) , strides = 1 , padding = 'same')(inputs)
+
+    x = Conv2D(32, (5,5) , strides = 1 , padding = 'same' )(inputs)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Conv2D(32, (3,3) , strides = 1 , padding ='same')(x)
+    x = Conv2D(32, (4,4) , strides = 1 , padding ='same' , kernel_regularizer=l2(0.01))(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((2,2) , strides =  2 , padding = 'same')(x)
@@ -102,7 +157,7 @@ def build_model(input_shape=(224, 224, 3), num_classes=3):  # Change num_classes
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = Conv2D(512, (1,1) , strides =1 , padding ='same')(x)
-    x = Dropout(0.45)(x)
+    x = Dropout(0.4)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling2D((2,2) , strides = 2 , padding ='same')(x)
@@ -113,22 +168,22 @@ def build_model(input_shape=(224, 224, 3), num_classes=3):  # Change num_classes
     x = Dense(512)(x)
     x = Dense(256)(x)
     x = Dense(64)(x)
-
-    outputs = Dense(num_classes, activation='softmax')(x)  # Set to 2 
+    outputs = Dense(num_classes, activation='softmax')(x)  # Set to 3 classes
     model = Model(inputs, outputs)
     return model
+'''
 # Compile the model
-model = build_model()
+model = build_modelv2()
 model.summary()
-model.compile(optimizer=Adam(learning_rate=1e-4), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=1e-4 , clipnorm =1.0), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 # Callbacks for training
-checkpoint = ModelCheckpoint('best_pneumonia_modelrgbposneg.keras', save_best_only=True, monitor='val_accuracy')
+checkpoint = ModelCheckpoint('best_pneumonia_modelrgbv2.keras', save_best_only=True, monitor='val_accuracy')
 early_stopping = EarlyStopping(monitor='val_accuracy', patience=12, restore_best_weights=True)
-reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.3, patience=2, cooldown=5)
+reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.2, patience=5, min_lr=1e-7, cooldown=3)
 # Train the model
 history = model.fit(
-    datagen.flow(X_train, y_train, batch_size=16),
-    epochs=100,
+    datagen.flow(X_train, y_train, batch_size=32),
+    epochs=150,
     validation_data=(X_val, y_val),
     callbacks=[checkpoint, early_stopping, reduce_lr]
 )
@@ -136,7 +191,7 @@ history = model.fit(
 test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
 print(f"\nTest accuracy: {test_acc}")
 # Load the best model
-best_model = tf.keras.models.load_model('best_pneumonia_modelrgbposneg.keras')
+best_model = tf.keras.models.load_model('best_pneumonia_modelrgbv2.keras')
 predictions = best_model.predict(X_test)
 predicted_classes = np.argmax(predictions, axis=1)
 # Classification report
